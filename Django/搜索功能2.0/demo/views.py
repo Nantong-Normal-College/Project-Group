03f3772d3fb5e2b25dba1a7fe44adf586c67a09d
Nice-yaya <1842933017@qq.com>
2021-11-19 17:57:51 +0800

    new

---
+++ Django/搜索功能2.0/demo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from . import service
+from demo.models import Shop
+# Create your views here.
+
+
+def search(request):
+    return render(request, "demo/search.html", {})
+
+
+def handle(request):
+    text = request.POST["搜索内容"]
+    service.addText(text)
+    db = Shop.objects.all()
+    po_list = []
+    for i in db:
+        if text in i.title:
+            po_list.append(i.content)
+
+    return render(request, "demo/resp.html", {"resp":  po_list})
+
+
+def hh(request):
+    return render(request, "demo/搜索页面.html", {})
